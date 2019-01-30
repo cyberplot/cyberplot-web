5f2ddde5032a2d1b4271eec92987cf968727b530
Tomires <me@tomires.eu>
2019-01-30 11:47:53 +0100

    Setup Flask

---
+++ cyberplot/cyberplot.py
@@ -0,0 +1,19 @@
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'cyberplot.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cyberplot',
+    packages=['cyberplot'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
