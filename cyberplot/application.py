1036d1ee418d7a87272ec9c732034b4a69efdc93
Tomires <me@tomires.eu>
2019-01-31 23:54:06 +0100

    Add dataset view

--- cyberplot/application.py
+++ cyberplot/application.py
@@ -8,3 +8,7 @@ bp = Blueprint('application', __name__)
 @bp.route('/')
 def index():
     return render_template('application/index.html')
+
+@bp.route('/dataset')
+def dataset():
+    return render_template('application/dataset.html')
