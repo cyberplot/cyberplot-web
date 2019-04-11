27b1b08556477b3d0c09a8d35d7a2e9e7d0d746a
Tomires <me@tomires.eu>
2019-04-11 18:35:36 +0900

    Change indent style, create login module

---
+++ frontend/cyberplot/src/components/Login.vue
@@ -0,0 +1,85 @@
+<template>
+<content>
+    <main>
+        <form id="login_form">
+            <img src="@/assets/images/logo_blue.svg" alt="Cyberplot logo">
+            <input type="text" name="user_name" placeholder="User name" class="textbox_with_icon" style="background-image: url('@/assets/images/icon_user_gray.svg');">
+            <input type="password" name="password" placeholder="Password" class="textbox_with_icon" style="background-image: url('@/assets/images/icon_password_gray.svg');">
+            <a href="#" id="button_login" class="button_primary">Log in</a>
+            <a href="#" id="button_forgot" class="button_secondary">Forgot password?</a>
+        </form>
+
+        <p>
+            New to cyberplot?
+            <a href="#" id="button_signup" class="button_secondary">Sign up</a>
+        </p>
+    </main>
+</content>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped>
+#login_form {
+    background-color: #f9f9f9;
+    padding: 1.5em;
+    border-radius: 0.3em;
+}
+
+content{
+    background: linear-gradient(135deg, #14e0f9, #3765ff);
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    overflow: auto;
+}
+
+main {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: block;
+    margin: 0;
+    position: relative;
+    width: 22em;
+    height: 24em;
+}
+
+#login_form img {
+    width: 12em;
+    display: block;
+    margin: auto;
+    margin-bottom: 2em;
+    margin-top: 1em;
+}
+
+#login_form .button_primary {
+    display: block;
+    text-align: center;
+}
+
+#login_form .button_secondary {
+    margin-top: 1em;
+    display: block;
+    text-align: center;
+}
+
+#button_signup {
+    margin-left: 0.5em;
+}
+
+p {
+    text-align: center;
+    color: white;
+}
+</style>
