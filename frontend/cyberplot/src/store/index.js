38a8ae2e6a9debe981b158de1f17951146a7c960
Tomires <me@tomires.eu>
2019-04-22 13:39:16 +0900

    Initialize vuex

---
+++ frontend/cyberplot/src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+const getters = {
+
+}
+
+const store = new Vuex.Store({
+    state,
+    actions,
+    mutations,
+    getters
+})
+
+export default store
