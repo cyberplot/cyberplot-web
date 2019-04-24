32b48e0a13c5f7948b2cafcd7e15c58719835068
Tomires <me@tomires.eu>
2019-04-24 19:17:10 +0900

    Add ability to select update method

--- frontend/cyberplot/src/components/Modals/DatasetUpdateModal.vue
+++ frontend/cyberplot/src/components/Modals/DatasetUpdateModal.vue
@@ -1,8 +1,8 @@
 <template>
 <div v-show="modalOpened">
-    <Base :updating="updating" />
-    <!--Python :updating="updating" />
-    <R :updating="updating" /-->
+    <Base :updating="updating" v-show="selectedMethod == METHODS.DEFAULT" v-on:formSubmit="selectUpdateMethod" />
+    <Python :updating="updating" v-show="selectedMethod == METHODS.PYTHON" />
+    <R :updating="updating" v-show="selectedMethod == METHODS.R" />
 </div>
 </template>
 
@@ -18,6 +18,23 @@ export default {
         Python,
         R
     },
+    data() {
+        return {
+            METHODS: {
+                DEFAULT: 'default',
+                LOCAL: 'local',
+                DATABASE: 'database',
+                PYTHON: 'python',
+                R: 'r'
+            },
+            selectedMethod: 'default'
+        }
+    },
+    methods: {
+        selectUpdateMethod: function(method) {
+            this.selectedMethod = method
+        }
+    },
     computed: {
         modalOpened() {
             return this.$store.state.openedModals.datasetUpdate
@@ -25,6 +42,13 @@ export default {
 
         updating() {
             return this.$store.state.datasetUpdateUpdating
+        }
+    },
+    watch: {
+        modalOpened: function(value, oldValue) {
+            if(oldValue === false && value === true) {
+                this.selectedMethod = this.METHODS.DEFAULT
+            }
         }
     }
 }
--- frontend/cyberplot/src/components/Modals/DatasetUpdateModal/Base.vue
+++ frontend/cyberplot/src/components/Modals/DatasetUpdateModal/Base.vue
@@ -10,39 +10,43 @@
     </span>
 
     <label>
-        <input type="radio" name="source" value="local">
+        <input type="radio" name="source" value="local" v-model="dataSource">
         <div class="button_secondary">
-            <img src="@/assets/images/icon_source_file_blue.svg">
+            <img src="@/assets/images/icon_source_file_white.svg" v-if="dataSource == 'local'">
+            <img src="@/assets/images/icon_source_file_blue.svg" v-else>
             Local file
         </div>
     </label>
 
     <label>
-        <input type="radio" name="source" value="local">
+        <input type="radio" name="source" value="database" v-model="dataSource">
         <div class="button_secondary">
-            <img src="@/assets/images/icon_source_database_blue.svg">
+            <img src="@/assets/images/icon_source_database_white.svg" v-if="dataSource == 'database'">
+            <img src="@/assets/images/icon_source_database_blue.svg" v-else>
             Remote database
         </div>
     </label>
 
     <label>
-        <input type="radio" name="source" value="local">
+        <input type="radio" name="source" value="python" v-model="dataSource">
         <div class="button_secondary">
-            <img src="@/assets/images/icon_source_python_blue.svg">
+            <img src="@/assets/images/icon_source_python_white.svg" v-if="dataSource == 'python'">
+            <img src="@/assets/images/icon_source_python_blue.svg" v-else>
             Python
         </div>
     </label>
     
     <label>
-        <input type="radio" name="source" value="local">
+        <input type="radio" name="source" value="r" v-model="dataSource">
         <div class="button_secondary">
-            <img src="@/assets/images/icon_source_r_blue.svg">
+            <img src="@/assets/images/icon_source_r_white.svg" v-if="dataSource == 'r'">
+            <img src="@/assets/images/icon_source_r_blue.svg" v-else>
             R data frame
         </div>
     </label>
 
     <nav>
-        <a href="#" id="button_next"><img src="@/assets/images/button_next.svg" alt="Next"></a>
+        <a href="#" id="button_next" @click="submitForm"><img src="@/assets/images/button_next.svg" alt="Next"></a>
         <a href="#" id="button_back"><img src="@/assets/images/button_back.svg" alt="Back"></a>
     </nav>
 </form>
@@ -52,7 +56,19 @@
 export default {
     name: 'DatasetUpdateModalBase',
     props: {
-        updating: Boolean
+        updating: Boolean,
+    },
+    data() {
+        return {
+            dataSource: ''
+        }
+    },
+    methods: {
+        submitForm: function() {
+            if(this.dataSource != '') {
+                this.$emit('formSubmit', this.dataSource)
+            }
+        }
     }
 }
 </script>
