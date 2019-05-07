a8ef638a7b76bf59ba35301a97c46b8d736ea12f
Tomires <me@tomires.eu>
2019-05-07 18:02:25 +0900

    Parametrize attribute rename modal

--- frontend/cyberplot/src/components/Modals/AttributeRenameModal.vue
+++ frontend/cyberplot/src/components/Modals/AttributeRenameModal.vue
@@ -1,9 +1,9 @@
 <template>
-<form id="form_rename_attribute" v-show="modalOpened">
+<form id="form_rename_attribute" v-show="modalOpened" v-if="currentDataset.dataset">
     <header><img src="@/assets/images/icon_rename_blue.svg"> Rename attribute</header>
-    <p>Please enter a new name for <strong>Sepal Length</strong>.</p>
+    <p>Please enter a new name for <strong>{{ attributeLabel }}</strong>.</p>
 
-    <input type="text" name="attribute_name" placeholder="Sepal Length">
+    <input type="text" name="attribute_name" :placeholder="attributeLabel">
     <a href="#" id="button_rename" class="button_primary">Rename attribute</a>
 </form>
 </template>
@@ -14,6 +14,18 @@ export default {
     computed: {
         modalOpened() {
             return this.$store.state.openedModals.attributeRename
+        },
+
+        selectedAttribute() {
+            return this.$store.state.selectedAttribute
+        },
+
+        currentDataset() {
+            return this.$store.state.currentDataset
+        },
+
+        attributeLabel() {
+            return this.currentDataset.attributes[this.selectedAttribute].label 
         }
     }
 }
