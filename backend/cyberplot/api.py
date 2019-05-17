21862b41b0770d2f8ffce2614428d867f6f09a23
Tomires <me@tomires.eu>
2019-05-17 17:25:32 +0900

    Implement dataset upload API

--- backend/cyberplot/api.py
+++ backend/cyberplot/api.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, jsonify, request
 from .models import db, User, Dataset, Space, Attribute, DatasetConnector, UserConnector, DatasetVersion, Statistics
 from .config import BaseConfig
+from .utils import isValidCSV, getDatasetData
 import simplejson as json
-import csv, itertools
+import csv, itertools, ast, werkzeug, os, datetime, secrets
 
 api = Blueprint("api", __name__)
 
@@ -16,7 +17,10 @@ def datasetList(uid):
 @api.route("/dataset/<int:uid>/<int:did>/")
 def dataset(uid, did):
     dataset = Dataset.query.filter_by(uid = uid, did = did, deleted = False).first().to_dict()
-    dataset["item_count"] = DatasetVersion.query.filter_by(did = did).order_by(DatasetVersion.vid.desc()).first().to_dict()["itemCount"]
+
+    lastVersion = DatasetVersion.query.filter_by(did = did).order_by(DatasetVersion.vid.desc()).first().to_dict()
+    dataset["item_count"] = lastVersion["itemCount"]
+    filename = lastVersion["filename"]
 
     attributes_original = Attribute.query.filter_by(uid = uid, did = did)
     attributes = []
@@ -26,7 +30,7 @@ def dataset(uid, did):
         new_attribute["values"] = []
         attributes.append(new_attribute)
 
-    with open('migrations/iris.csv') as csvfile:    
+    with open(filename) as csvfile:    
         reader = csv.reader(csvfile)
         for i, row in enumerate(itertools.islice(reader, BaseConfig.API_ATTRIBUTE_VALUE_PREVIEW_LENGTH + 1)):
             if i == 0:
@@ -45,3 +49,97 @@ def dataset(uid, did):
 def userAutocomplete(phrase):
     users = User.query.filter(User.username.startswith(phrase)).limit(5).with_entities(User.uid, User.username)
     return jsonify({ 'users': [u for u in users] })
+
+@api.route("/upload/", methods = ("POST",))
+def uploadDataset():
+    RequestForm = request.form
+    data = RequestForm.to_dict(flat = False)
+    metadata = str(data["json"][0])
+    metadataDictionary = ast.literal_eval(metadata)["json"]
+
+    createDataset = False
+    apiKey = metadataDictionary["identifier"]
+    connector = DatasetConnector.query.filter_by(key = apiKey).first()
+
+    # if API key isn't associated with an existing dataset, 
+    # presume we are creating a new table and look for user's API key
+    if not connector: 
+        connector = UserConnector.query.filter_by(key = apiKey).first()
+        createDataset = True
+
+        # if it's not there either, give up
+        if not connector:
+            return jsonify({'result': 'Provided API key is not valid.'}), 406
+
+    userID = connector.to_dict()["UID"]
+    datasetID = 0
+    
+    if createDataset:
+        datasetID = Dataset.query.filter_by(uid = userID).order_by(Dataset.did.desc()).first().to_dict()["DID"] + 1
+    else:
+        datasetID = connector.to_dict()["DID"]
+
+    filename = "datasets/" + str(userID) + "/" + str(datasetID) + "/data.csv"
+
+    # save file
+    datasetData = request.files["file"]
+    os.makedirs(os.path.dirname(filename), exist_ok = True)
+    datasetData.save(filename)
+
+    if not isValidCSV(filename):
+        os.unlink(filename)
+        return jsonify({'result': 'Provided file is not a valid CSV file.'}), 406
+
+    if createDataset:
+        datasetName = metadataDictionary["name"]
+        if not datasetName:
+            os.unlink(filename)
+            return jsonify({'result': 'Please provide a dataset name.'}), 406
+        
+        # get file data, attributes, item count, statistics
+        datasetData = getDatasetData(filename)
+
+        newDataset = Dataset(uid = userID,
+                             did = datasetID,
+                             name = datasetName,
+                             last_edit = datetime.datetime.now(),
+                             deleted = False)
+        db.session.add(newDataset)
+
+        newDatasetVersion = DatasetVersion(vid = 1,
+                                           uid = userID,
+                                           did = datasetID,
+                                           filename = filename,
+                                           upload_date = datetime.datetime.now(),
+                                           item_count = datasetData["item_count"])
+        db.session.add(newDatasetVersion)
+
+        for i, attribute in enumerate(datasetData["attributes"]):
+            attribute.uid = userID
+            attribute.did = datasetID
+            attribute.aid = i + 1 # SQL increments from 1
+            attribute.missing_value_setting = 0
+            db.session.add(attribute)
+
+        for statistics in datasetData["statistics"]:
+            statistics.uid = userID
+            statistics.did = datasetID
+            db.session.add(statistics)
+        
+        # generate API key for dataset
+        while True:
+            key = secrets.token_hex(nbytes = 16)
+
+            # make sure we generate a unique key
+            if not DatasetConnector.query.filter_by(key = key).first():
+                if not UserConnector.query.filter_by(key = key).first():
+                    connector = DatasetConnector(uid = userID, did = datasetID, key = key)
+                    db.session.add(connector)
+                    break
+
+        db.session.commit()
+
+    else:
+        print("update") #TODO
+
+    return jsonify({'result': True}), 201
--- backend/cyberplot/utils.py
+++ backend/cyberplot/utils.py
@@ -1,4 +1,5 @@
-import enum
+import enum, csv, itertools
+from .config import BaseConfig
 
 class attributeTypes(enum.Enum):
     NOMINAL = 1
@@ -7,10 +8,76 @@ class attributeTypes(enum.Enum):
     VECTOR = 4
 
 def isFlagOnPosition(mask, pos):
-    return ((mask >> pos) & 1) != 0
+    return ((mask >> pos - 1) & 1) != 0
+
+def flipBitOnPosition(mask, pos):
+    return mask ^ (1 << pos - 1)
 
 def intToType(int):
     return attributeTypes(int).name
 
-def typeToint(type):
-    return attributeTypes[type].value
+def typeToInt(type):
+    return type.value
+
+def isValidCSV(filename):
+    # TODO
+    return True
+
+def getDatasetData(filename):
+    data = {}
+
+    data["attributes"] = []
+    from .models import Attribute
+
+    with open(filename) as csvfile:    
+        reader = csv.reader(csvfile)
+
+        for i, row in enumerate(reader):
+            if i == 0: # first row contains labels
+                for label in row:
+                    data["attributes"].append(Attribute(label = label, 
+                                                        missing_value_setting = 0,
+                                                        type_mask = pow(2, len(attributeTypes)) - 1,
+                                                        type = typeToInt(attributeTypes.NUMERICAL)))
+                continue
+    
+    import numpy as np
+    dataset = np.genfromtxt(filename, delimiter = ",", skip_header = 1, usemask = True)
+    np.warnings.filterwarnings("ignore")
+    data["item_count"] = len(dataset)
+
+    median = np.median(dataset, axis = 0)
+    mean = np.mean(dataset, axis = 0)
+    sdev = np.std(dataset, axis = 0)
+    minimum = np.quantile(dataset, 0, axis = 0)
+    q1 = np.quantile(dataset, .25, axis = 0)
+    q3 = np.quantile(dataset, .75, axis = 0)
+    maximum = np.quantile(dataset, 1, axis = 0)
+
+    datasetStrings = np.genfromtxt(filename, delimiter = ",", skip_header = 1, usemask = True, dtype = str)
+
+    data["statistics"] = []
+    from .models import Statistics
+
+    for i in range(len(data["attributes"])):
+        # TODO ignore vectors for now
+        data["attributes"][i].type_mask = flipBitOnPosition(data["attributes"][i].type_mask, typeToInt(attributeTypes.VECTOR))
+        
+        if np.isnan(median[i]):
+            data["attributes"][i].type_mask = flipBitOnPosition(data["attributes"][i].type_mask, typeToInt(attributeTypes.NUMERICAL))
+            if len(np.unique(datasetStrings[:,i])) <= BaseConfig.ATTRIBUTE_CLASSIFICATION_CATEGORY_MAX_UNIQUE_COUNT:
+                data["attributes"][i].type = typeToInt(attributeTypes.CATEGORICAL)
+            else:
+                data["attributes"][i].type = typeToInt(attributeTypes.NOMINAL)
+            continue
+        
+        data["statistics"].append(Statistics(aid = i + 1, # SQL increments from 1
+                                             minimum = minimum[i],
+                                             q1 = q1[i],
+                                             median = median[i],
+                                             q3 = q3[i],
+                                             maximum = maximum[i],
+                                             mean = mean[i],
+                                             sdev = sdev[i]))
+
+    return data
