15e698473c642feb64d97b69dd9dcd1f910df34b
Tomires <me@tomires.eu>
2019-05-17 17:20:06 +0900

    Trim stats items to one decimal place

--- frontend/cyberplot/src/components/DatasetAttributeStatistics.vue
+++ frontend/cyberplot/src/components/DatasetAttributeStatistics.vue
@@ -4,13 +4,13 @@
         <dt><img src="@/assets/images/icon_statistics_gray.svg"> Statistics</dt>
         <dd>
             <table>
-                <tr><td>{{ currentDataset.statistics[selectedAttribute].minimum }}</td><td>{{ currentDataset.statistics[selectedAttribute].Q1 }}</td><td>{{ currentDataset.statistics[selectedAttribute].median }}</td><td>{{ currentDataset.statistics[selectedAttribute].Q3 }}</td><td>{{ currentDataset.statistics[selectedAttribute].maximum }}</td></tr>
+                <tr><td>{{ currentDataset.statistics[selectedAttribute].minimum.toFixed(1) }}</td><td>{{ currentDataset.statistics[selectedAttribute].Q1.toFixed(1) }}</td><td>{{ currentDataset.statistics[selectedAttribute].median.toFixed(1) }}</td><td>{{ currentDataset.statistics[selectedAttribute].Q3.toFixed(1) }}</td><td>{{ currentDataset.statistics[selectedAttribute].maximum.toFixed(1) }}</td></tr>
                 <tr><td>min</td><td>1Q</td><td>med</td><td>3Q</td><td>max</td></tr>
             </table>
 
             <ul>
-                <li><img src="@/assets/images/icon_mean_gray.svg" alt="Arithmetic mean"> {{ currentDataset.statistics[selectedAttribute].mean }}</li>
-                <li><img src="@/assets/images/icon_standard_deviation_gray.svg" alt="Standard deviation"> {{ currentDataset.statistics[selectedAttribute].sdev }}</li>
+                <li><img src="@/assets/images/icon_mean_gray.svg" alt="Arithmetic mean"> {{ currentDataset.statistics[selectedAttribute].mean.toFixed(2) }}</li>
+                <li><img src="@/assets/images/icon_standard_deviation_gray.svg" alt="Standard deviation"> {{ currentDataset.statistics[selectedAttribute].sdev.toFixed(2) }}</li>
             </ul>
         </dd>
     </dl>
