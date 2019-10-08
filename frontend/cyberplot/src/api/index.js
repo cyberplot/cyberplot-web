5af97caed4eefcd2c3cb5b61ddf349d230826f05
Tomires <me@tomires.eu>
2019-10-09 00:48:55 +0200

    Modify API URL to be an environmental variable

--- frontend/cyberplot/src/api/index.js
+++ frontend/cyberplot/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://127.0.0.1:5000/api'
+const API_URL = process.env.API_URL
 
 /* when login token expires, redirect user to login */
 axios.interceptors.response.use(
